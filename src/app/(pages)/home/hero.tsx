62056f708fff24a6a5e96e5d9693043f2cc61158
mmriz16 <m.miftakhul.rizky4@gmail.com>
2025-12-10 11:21:26 +0700

    feat: add hero section for home page and new abouts page

--- src/app/(pages)/home/hero.tsx
+++ src/app/(pages)/home/hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
         </div>
         <div className="col-span-10">
           <p id='desc' className="text-sm md:text-base font-medium text-[var(--text-secondary)]">
-            I&apos;m <strong>Miftakhul Rizky</strong>, a passionate UI/UX Designer and Icon Designer based in Bandung City.
+            I&apos;m <strong>Miftakhul Rizky</strong>, a passionate UI/UX Designer and Icon Designer based in Jakarta City.
             As the founder and CEO of <strong>Termicons</strong>, I create intuitive digital experiences and beautiful iconography
             that bridges functionality with aesthetics. Contact me at m.miftakhul.rizky4@gmail.com for collaborations.
           </p>
